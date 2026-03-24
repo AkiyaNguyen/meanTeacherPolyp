b10d276fed8689dc762010c5333d450760211711
Nguyen Huu Phuoc <pn4358189@gmail.com>
2026-03-24 23:54:25 +0700

    change sth

--- utils/build_dataset.py
+++ utils/build_dataset.py
@@ -51,7 +51,11 @@ def build_dataset(cfg):
         train_files = all_files[val_num:]
         train_data = getattr(dataset, cfg.get('data.dataset'))(
             root=cfg.get('data.root'), data2_dir=cfg.get('data.data2_dir'),
-            mode='train', require_depth=cfg.get('data.require_depth'), list_name=train_files)
+            mode='train', require_depth=cfg.get('data.require_depth'),
+            image_dirname=cfg.get('data.image_dirname'),
+            mask_dirname=cfg.get('data.mask_dirname'),
+            depth_dirname=cfg.get('data.depth_dirname'),
+             list_name=train_files)
         if cfg.get('data.label_mode') == 'percentage':
             labeled_num = round(train_num * cfg.get('data.labeled_perc') / 100)
             if labeled_num % 2 != 0:
@@ -68,19 +72,20 @@ def build_dataset(cfg):
         train_dataset_root = os.path.join(cfg.get('data.root'), cfg.get('data.data2_dir'))
         val_data = ImageFolderDataset(
             dataset_root=train_dataset_root,
-            image_dirname='images', mask_dirname='masks', depth_dirname='depth-v1',
+            image_dirname=cfg.get('data.test.image_dirname'),
+            mask_dirname=cfg.get('data.test.mask_dirname'),
+            depth_dirname=cfg.get('data.test.depth_dirname'),
             transform=val_test_transform, list_name=val_files)
         val_dataloader = torch.utils.data.DataLoader(
             val_data, batch_size=cfg.get('data.test.batch_size'), shuffle=False, num_workers=0)
     else:
         raise ValueError("val_perc must be between 0 and 100.")
 
-    depth_dir = str(cfg.get('data.test.depth_dirname')) if cfg.get('data.test.require_depth') else None
     test_data = ImageFolderDataset(
         dataset_root=cfg.get('data.test.dataset_root'),
         image_dirname=cfg.get('data.test.image_dirname'),
-        mask_dirname=str(cfg.get('data.test.mask_dirname')),
-        depth_dirname=str(depth_dir),
+        mask_dirname=cfg.get('data.test.mask_dirname'),
+        depth_dirname=cfg.get('data.test.depth_dirname'),
         transform=val_test_transform, list_name=None)
 
     test_dataloader = torch.utils.data.DataLoader(
