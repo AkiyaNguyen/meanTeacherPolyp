a5266d37630db490e5e3b838c518d04bc876cde7
hikito-otikih <huynhchiton981@gmail.com>
2026-03-28 16:28:59 +0700

    fix cfg

--- emaEncoderOnlyTrain_imageAsDepth_res18.py
+++ emaEncoderOnlyTrain_imageAsDepth_res18.py
@@ -396,7 +396,7 @@ def training(cfg: Config, trial: typing.Optional[optuna.trial.Trial] = None):
     tea_model = getattr(models, cfg.get('model.tea_model.name'))(num_classes=cfg.get('model.num_channels_output')).to(device)
 
     trainer_cfg = cfg.get('Trainer', {})
-    depth_encoder_ckpt = trainer_cfg.get('depth_encoder_ckpt', 'pretrained/encoder.pth')
+    depth_encoder_ckpt = trainer_cfg.get('depth_encoder_ckpt')
     # depth_encoder_ckpt = cfg.get('Trainer.depth_encoder_ckpt', 'pretrained/encoder.pth')
     load_pretrained_resnet18_depth_encoder(tea_model, depth_encoder_ckpt)
     tea_model = tea_model.to(device)
